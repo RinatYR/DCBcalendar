dfe5ef6cf88d1fae8194b3d00f818eca866c7dfc
Rinat <ceo@netmoon.ru>
2022-05-22 12:55:38 +0300

    disable eslin

--- dcb_calendar/dcb_calendar_ui/lint-staged.config.js
+++ dcb_calendar/dcb_calendar_ui/lint-staged.config.js
@@ -3,5 +3,5 @@ module.exports = {
     '*.{ts,tsx}': [() => 'tsc --noEmit'],
     '*.{ts,tsx,less}': ['npm run prettier:pre-commit'],
     'src/**/*.less': ['npm run stylelint:pre-commit'],
-    '{src,__tests__}/**/*.{ts,tsx}': ['npm run eslint:pre-commit'],
+    // '{src,__tests__}/**/*.{ts,tsx}': ['npm run eslint:pre-commit'],
 };
