1aeb249f85ebbf3832078e9967ffbb77ca12b818
Rinat <ceo@netmoon.ru>
2022-11-18 11:27:41 +0500

    fix small height screen

--- dcb_calendar/dcb_calendar_ui/src/Components/Header/Header.tsx
+++ dcb_calendar/dcb_calendar_ui/src/Components/Header/Header.tsx
@@ -13,23 +13,25 @@ export const Header: React.FC = () => {
   return (
     <>
       <header className={style.header}>
-        <a href="/" className="headerLogo">
-          <img src="./static/img/logo.svg" alt="logo" className="headerLogoImg" />
-        </a>
-        <Search />
-        <div className={style.headerButtons}>
-          <button
-            className={style.headerButtonWhite}
-            onClick={() => setShowBecomeSpeakerForm(true)}
-          >
-            Стать спикером
-          </button>
-          <button className={style.headerButtonWhite} onClick={() => setShowPartAwardForm(true)}>
-            Участвовать в премии
-          </button>
-          <button className={style.headerButton} onClick={() => setShowSuggestEventForm(true)}>
-            Предложить событие
-          </button>
+        <div className={style.headerContainer}>
+          <a href="/" className={style.headerLogo}>
+            <img src="./static/img/logo.svg" alt="logo" className={style.headerLogoImg} />
+          </a>
+          <Search />
+          <div className={style.headerButtons}>
+            <button
+              className={style.headerButtonWhite}
+              onClick={() => setShowBecomeSpeakerForm(true)}
+            >
+              Стать спикером
+            </button>
+            <button className={style.headerButtonWhite} onClick={() => setShowPartAwardForm(true)}>
+              Участвовать в премии
+            </button>
+            <button className={style.headerButton} onClick={() => setShowSuggestEventForm(true)}>
+              Предложить событие
+            </button>
+          </div>
         </div>
       </header>
       <BecomeSpeakerForm
